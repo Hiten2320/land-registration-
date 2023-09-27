ba231d6c37c7ff7a7c1aa05aa4bd6af897e33b6e
Hiten2320 <97287153+Hiten2320@users.noreply.github.com>
2023-09-27 21:47:53 +0530

    Add to remote Github

---
+++ lib/constant/constants.dart
@@ -0,0 +1,11 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDY4NDA3NmU2QzQ5YzZjYTUzMmMwYmVmQjFiYzJFNTA1OWYzMzU4MEYiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4NjQxMzUzNTU0MSwibmFtZSI6ImxhbmRfcmVnaXN0cnkifQ.wKNFJOvSZSKWB4xc4yw7jhAJK4uNsQpyv3rrugWiszA';
+
+const String rpcUrl = "http://127.0.0.1:7545";
+
+const String contractAddress = "0x5131aabe6478630bad85097c1de1cc97bfb4734b";
+
+const int chainId = 1337; // local ganache-1337
